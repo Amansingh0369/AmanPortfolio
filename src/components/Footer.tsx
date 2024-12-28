2486fa2e8d231e2cc3d297cc30bc7c53e33b21d7
Aman Singh <101695980+Amansingh0369@users.noreply.github.com>
2024-12-29 02:57:50 +0530

    added navbar added footer added mousecircle

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const VignetteDiv: React.FC = () => {
+    return (
+        <div className="p-6 bg-black h-[30rem] border">
+            <div className="h-full text-6xl text-white">
+                <div className="bg-gradient-to-t from-transparent to-black bg-clip-text text-white">
+                    <h1>Get in touch</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VignetteDiv;
